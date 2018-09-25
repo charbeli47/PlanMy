95b7a14be1e1b0ad6beceb7de479312dc8a09c9a
Charbel Issa <charbel_i@hotmail.com>
2018-09-25 17:43:31 +0300

    Add project files.

---
+++ Design/ftml-www/wpf/App Format - Login Page/App.xaml.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace App Format - Login Page
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+	/// Generated from Export Kit - Lightning Storm
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
+	
